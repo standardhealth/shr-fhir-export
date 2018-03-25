7c62639aa6ab6de309c1a8d016c0a7e47bb32938
Chris Moesel <cmoesel@mitre.org>
2018-03-24 21:03:20 -0400

    Models: Support for nested type constraints

--- test/export_models_tests.js
+++ test/export_models_tests.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const err = require('shr-test-helpers/errors');
 const {sanityCheckModules} = require('shr-models');
@@ -9,10 +9,26 @@ const {ModelsExporter, setLogger} = require('../lib/logical/export');
 
 sanityCheckModules({ 'shr-test-helpers': export_tests });
 
+// define the fixFn to pass in to auto-fix broken tests
+function fixFn (name, result, errors) {
+  if (/^\s*(true|yes|1)\s*$/i.test(process.env.FIX_TEST_ERRORS)) {
+    if (result != null) {
+      const fixture = path.join(`${__dirname}/fixtures/`, `${name}.json`);
+      console.error(`Fixing ${name} expected fixture to actual result.  Check ${fixture}.`);
+      fs.writeFileSync(fixture, JSON.stringify(result, null, 2));
+    }
+    if (errors.length) {
+      const fixture_err = path.join(`${__dirname}/fixtures/`, `${name}_errors.json`);
+      console.error(`Fixing ${name} expected errors fixture to actual errors.  Check ${fixture_err}.`);
+      fs.writeFileSync(fixture_err, JSON.stringify(errors.map(e => ({ msg: e.msg })), null, 2));
+    }
+  }
+}
+
 // Set the logger -- this is needed for detecting and checking errors
 setLogger(err.logger());
 
-describe('#exportToJSON()', commonExportTests(exportSpecifications, importFixture, importErrorsFixture, path.join(__dirname, 'actuals')));
+describe('#exportToJSON()', commonExportTests(exportSpecifications, importFixture, importErrorsFixture, fixFn, path.join(__dirname, 'actuals')));
 
 function defaultConfiguration()
 {
